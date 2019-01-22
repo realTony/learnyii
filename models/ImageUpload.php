da6f8db3b972f35627618857fc96dae55e119457
realTony <whitewolf071@gmail.com>
2019-01-22 23:59:09 +0200

    Upload fix

--- models/ImageUpload.php
+++ models/ImageUpload.php
@@ -128,8 +128,8 @@ class ImageUpload extends Model
     {
         if( !is_dir($path ) )
         {
-            $path = Yii::getAlias('@webroot').$path;
-            FileHelper::createDirectory($path, '0775' );
+            $realpath = Yii::getAlias('@webroot').$path;
+            FileHelper::createDirectory($realpath, '0775' );
             return $path;
         }
 
