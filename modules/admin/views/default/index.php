2a01077a15cf5e64b80f73845e2790600795de32
realTony <whitewolf071@gmail.com>
2019-02-09 03:23:25 +0200

    Night update

--- modules/admin/views/default/index.php
+++ modules/admin/views/default/index.php
@@ -2,16 +2,6 @@
 $this->title = 'Главная страница';
 ?>
 <div class="admin-default-index">
-<!--    <h1>--><?//= $this->context->action->uniqueId ?><!--</h1>-->
-<!--    <p>-->
-<!--        This is the view content for action "--><?//= $this->context->action->id ?><!--".-->
-<!--        The action belongs to the controller "--><?//= get_class($this->context) ?><!--"-->
-<!--        in the "--><?//= $this->context->module->id ?><!--" module.-->
-<!--    </p>-->
-<!--    <p>-->
-<!--        You may customize this page by editing the following file:<br>-->
-<!--        <code>--><?//= __FILE__ ?><!--</code>-->
-<!--    </p>-->
     <div class="row">
         <div class="col-md-6">
             <div class="card bg-white m-b">
