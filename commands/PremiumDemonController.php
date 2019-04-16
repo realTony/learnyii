e8bd5546589405640f1e7a72d6c8dd70359ab7fe
realTony <whitewolf071@gmail.com>
2019-04-16 19:58:41 +0300

    Local changes

--- commands/PremiumDemonController.php
+++ commands/PremiumDemonController.php
@@ -4,6 +4,7 @@ namespace app\commands;
 
 
 use app\models\AdvertisementPost;
+use app\models\PremiumRates;
 use app\models\Settings;
 use app\models\UserPremiumAdvertisement;
 use Yii;
@@ -41,39 +42,42 @@ class PremiumDemonController extends Controller
         $startTime = microtime(true);
         $endTime = false;
         $userPurchases = new UserPremiumAdvertisement();
-        $currentDate = date('Y-m-d H:i:s', time());
+        $settings = new Settings();
+        $currentDate = (new \DateTime());
         $expiredPosts  = $userPurchases
             ->find()
             ->where(['not', ['confirmation_timestamp' => null, 'expiration_timestamp' => null]])
             ->andWhere(['is_notification_sent' => 0])
-            ->andWhere(['<', 'confirmation_timestamp', $currentDate])
-            ->andWhere(['>', 'expiration_timestamp', $currentDate] )
+            ->andWhere(['<', 'confirmation_timestamp', $currentDate->format('Y-m-d H:i:s')])
+            ->andWhere(['>', 'expiration_timestamp', $currentDate->format('Y-m-d H:i:s')] )
             ->all();
 
         foreach ($expiredPosts as $userPost) {
-            $userEmail = Yii::$app->db
-                ->createCommand('SELECT `email` FROM {{%user}} WHERE id=:id')
-                ->bindValue(':id', $userPost->author_id)
-            ->queryOne();
+            $rate = ( new PremiumRates())
+                ->findOne(['id' => $userPost->premium_type_id]);
+            $compareDate = ($rate->duration > 24) ?
+                    (new \DateTime($userPost->expiration_timestamp))->modify('-24 hours') : (new \DateTime($userPost->expiration_timestamp))->modify('-12 hours');
 
-            $settings = new Settings();
+            if( $compareDate <= $currentDate) {
 
-                $status = Yii::$app->mailer->compose()
-                    ->setFrom($settings->siteEmail->option_value)
-                    ->setTo($userEmail['email'])
-                    ->setSubject('Уведомление')
-                    ->setHtmlBody($settings->emailNotification->option_value)
-                    ->send();
+                 $userEmail = Yii::$app->db
+                     ->createCommand('SELECT `email` FROM {{%user}} WHERE id=:id')
+                     ->bindValue(':id', $userPost->author_id)
+                 ->queryOne();
+                 $status = Yii::$app->mailer->compose()
+                     ->setFrom($settings->siteEmail->option_value)
+                     ->setTo($userEmail['email'])
+                     ->setSubject('Уведомление')
+                     ->setHtmlBody($settings->emailNotification->option_value)
+                     ->send();
 
-                $res = Yii::$app->db
-                    ->createCommand('UPDATE {{%user_premium_advertisement}} SET `is_notification_sent` = 1
-                    WHERE `id`=:id')
-                    ->bindValue(':id',$userPost->id)
-                    ->query();
+                 Yii::$app->db->createCommand('UPDATE {{%user_premium_advertisement}} SET `is_notification_sent` = 1 WHERE `id`=:id')
+                 ->bindValue(':id',$userPost->id)
+                 ->query();
+            }
 
         }
-        $endTime = microtime(true);
 
-        echo 'Processing for '.($endTime - $startTime).' seconds';
+        $endTime = microtime(true);
     }
 }
