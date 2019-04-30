a7d6e7ddba39db157d2aef4f38a47724cc317c66
realTony <whitewolf071@gmail.com>
2019-04-30 05:20:44 +0300

    Js filters fix

--- web/js/jquery.main.js
+++ web/js/jquery.main.js
@@ -1,3 +1,14 @@
+$(document).on('load ready', function (e) {
+    resolutions();
+    resolutions1();
+    resolutions2();
+    resolutions3();
+    init_and_resize();
+    init_and_resize1();
+    init_and_resize2();
+    init_and_resize3();
+});
+
 $(document).ready(function(){
     window.mobilecheck = function() {
         var check = false;
@@ -9,14 +20,6 @@ $(document).ready(function(){
         $('.slider-arrow').css({opacity:1});
     }
 
-    resolutions();
-    resolutions1();
-    resolutions2();
-    resolutions3();
-    init_and_resize();
-    init_and_resize1();
-    init_and_resize2();
-    init_and_resize3();
     $(window).resize(function() {
         init_and_resize();
         init_and_resize1();
@@ -772,8 +775,8 @@ function init_and_resize3(){
     if($('.resolutions768').is(':visible')){
         $('.content .title-text').append($('.holder-aside-right'));
         $('.info-car').prepend($('.aside-profile'));
-        //$('.holder-filters .block-filter').append($('.holder-aside-left'));
-        //$('.holder-filters .block-filter').prepend($('.form-content'));
+        $('.holder-filters .block-filter').append($('.holder-aside-left'));
+        $('.holder-filters .block-filter').prepend($('.form-content'));
         // $('.block-filter').prepend($('.aside-profile'));
         //$('.holder-information .block-filter').prepend($('.aside-profile'));
     }
