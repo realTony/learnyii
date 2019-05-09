e270aecfad6528b0cf67e7eddd236b14a35d48e2
realTony <whitewolf071@gmail.com>
2019-05-10 01:59:30 +0300

    W3C Validator fixes

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -14,8 +14,7 @@ $this->beginPage();
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta http-equiv="Content-Type" content="text/html">
+    <meta http-equiv="Content-Type" content="text/html; charset=<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
