d3b20338045d7fe918df9db2d71a6fa292750bf7
realTony <whitewolf071@gmail.com>
2019-08-07 19:11:48 +0300

    5d4abc17ede47e8d9f686a9c Оповещение при размещении объявления

---
+++ components/SendNotification.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace app\components;
+
+
+use Yii;
+use yii\base\Model;
+
+class SendNotification extends Model
+{
+    public static function sendNotification($email, $messageTxt)
+    {
+        $message = Yii::$app->mailer->compose();
+
+        $message->setTo(Yii::$app->params['adminEmail'])
+                ->setSubject(Yii::t('app', Yii::$app->name))
+                ->setTextBody($messageTxt)
+                ->send();
+    }
+
+}
